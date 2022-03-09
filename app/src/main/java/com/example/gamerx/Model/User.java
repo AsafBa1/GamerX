ab297c6732670cded0de75f9bca0c0569b091e55
asaf2 <asaf2605@gmail.com>
2022-03-09 22:56:59 +0200

    update

---
+++ app/src/main/java/com/example/gamerx/Model/User.java
@@ -0,0 +1,13 @@
+package com.example.gamerx.Model;
+
+public class User {
+    public String Username,email;
+
+    public User(){
+
+    }
+    public User(String username,String email){
+        this.Username = username;
+        this.email = email;
+    }
+}
