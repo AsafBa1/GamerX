0f865ca651a5c0a0dd4e6cfc17550717a94f502d
asaf2 <asaf2605@gmail.com>
2022-03-12 22:59:08 +0200

    update

--- app/src/main/java/com/example/gamerx/MainActivity.java
+++ app/src/main/java/com/example/gamerx/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
@@ -19,11 +20,13 @@ import com.example.gamerx.Model.Post;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    List<Post> data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        data = Model.instance.getAllPosts();
         RecyclerView list = findViewById(R.id.posts_list_rv);
         list.setHasFixedSize(true);
         list.setLayoutManager(new LinearLayoutManager(this));
@@ -32,9 +35,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView titleTv;
+        TextView infoTv;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            titleTv = itemView.findViewById(R.id.row_title);
+            infoTv = itemView.findViewById(R.id.row_post_info);
         }
     }
     class MyAdapter extends RecyclerView.Adapter<MyViewHolder>{
@@ -42,7 +49,9 @@ public class MainActivity extends AppCompatActivity {
         @NonNull
         @Override
         public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return null;
+            View view = getLayoutInflater().inflate(R.layout.post_row,parent,false);
+            MyViewHolder holder = new MyViewHolder(view);
+            return holder;
         }
 
         @Override
@@ -52,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getItemCount() {
-            return 0;
+            return data.size();
         }
     }
 
