e883277fcf98e7b27c3b44e38688d857ec5605e6
asaf2 <asaf2605@gmail.com>
2022-03-19 16:34:32 +0200

    update

--- app/src/main/java/com/example/gamerx/NewPost.java
+++ app/src/main/java/com/example/gamerx/NewPost.java
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +15,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class NewPost extends Fragment {
-    TextView titleTv;
-    EditText bodyTv;
-    Button nextBtn;
-    private String title;
+    EditText titleEt;
+    EditText bodyEt;
+    EditText postId;
+    Button saveBtn;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,25 +27,22 @@ public class NewPost extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_new_post, container, false);
 
-        titleTv = view.findViewById(R.id.new_title_post);
-        nextBtn = view.findViewById(R.id.save_new_post);
-        nextBtn.setOnClickListener(new View.OnClickListener() {
+        postId = view.findViewById(R.id.new_post_id);
+        titleEt = view.findViewById(R.id.new_title_post);
+        bodyEt = view.findViewById(R.id.new_post_body);
+        saveBtn = view.findViewById(R.id.save_new_post);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                titleTv.setText("_________");
-            }
-        });
-        if(title != null){
-            titleTv.setText(title);
-        }
+            public void onClick(View v) { save(); }});
         //setHasOptionsMenu(true);
         return view;
     }
 
-    public void setTitle(String title) {
-        if(titleTv != null){
-            titleTv.setText(title);
-        }
-        this.title = title;
+    private void save() {
+        String id = postId.getText().toString();
+        String title = titleEt.getText().toString();
+        String body = bodyEt.getText().toString();
+        Log.d("TAG","saved name:" + title);
     }
+
 }
